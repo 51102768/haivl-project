303d5db7c17681bb11cf860811edfb3ef8de7344
ntq170493 <nt.quan1704@gmail.com>
2016-01-14 08:58:42 +0700

    change database settings

--- haivl_project/settings.py
+++ haivl_project/settings.py
@@ -78,11 +78,8 @@ WSGI_APPLICATION = 'haivl_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'haivldb',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
